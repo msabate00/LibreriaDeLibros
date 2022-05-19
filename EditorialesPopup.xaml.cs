a248d147e4ae89aeee9a66029c0a088c3073fe5b
msabate00 <sabatemiranda@gmail.com>
2022-05-19 20:56:15 +0200

    MSM - Sistema de provincias / municipios hecho

--- EditorialesPopup.xaml.cs
+++ EditorialesPopup.xaml.cs
@@ -22,22 +22,44 @@ namespace LibreriaDeLibrosSL
     {
         private forma _forma;
         private int id_tratar;
+        private List<Provincia> provincias;
         public enum forma { 
             insertar = 0, modificar = 1, eliminar = 2
         }
         public EditorialesPopup(forma f)
         {
             _forma = f;
-            if (_forma == forma.insertar) {
+            
+            InitializeComponent();
+            if (_forma == forma.insertar)
+            {
                 tb_editorial_popup_id.IsEnabled = false;
             }
-            InitializeComponent();
+            cargar();
         }
         public EditorialesPopup(forma f, int id)
         {
             _forma = f;
             id_tratar = id;
             InitializeComponent();
+            cargar();
+        }
+
+        private void cargar() {
+            Connexio connexio = new Connexio();
+            string query = "SELECT id,provincia from provincies";
+            MySqlCommand cmd = new MySqlCommand(query, connexio.Conn);
+            MySqlDataReader rdr = cmd.ExecuteReader();
+            provincias = new List<Provincia>();
+
+            while (rdr.Read())
+            {
+                Provincia p = new Provincia(rdr.GetInt32(0), rdr.GetString(1));
+                provincias.Add(p);
+            }
+            cb_editorial_popup_provincia.ItemsSource = provincias;
+            
+            connexio.Close();
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
@@ -47,9 +69,12 @@ namespace LibreriaDeLibrosSL
                 case forma.insertar:
                     //INSERT INTO editoriales (cif, nombre, telefono, direccion, codi_postal, codi_provincia , codi_municipi, email) 
                     //VALUES('U23747413', 'Áglaya', '968320680', 'C/ Real, 16 - Bajo', 30201, 44, 244127, 'info@editorialaglaya.com');
-
+                    //'U23747413', 'Áglaya', '968320680', 'C/ Real, 16 - Bajo', 30201, 44, 244127, 'info@editorialaglaya.com'
                     string query = "INSERT INTO editoriales (cif, nombre, telefono, direccion, codi_postal, codi_provincia , codi_municipi, email) " +
-                        "VALUES()";
+                        "VALUES('"+tb_editorial_popup_cif.Text+ "','" + tb_editorial_popup_nombre.Text + "'" +
+                        "'" + tb_editorial_popup_telefono.Text + "''" + tb_editorial_popup_direccion.Text + "'" +
+                        "'" + tb_editorial_popup_cp.Text + "''" + tb_editorial_popup_cif.Text + "'" +
+                        "'" + tb_editorial_popup_cif.Text + "''" + tb_editorial_popup_cif.Text + "')";
                     MySqlCommand cmd = new MySqlCommand(query, connexio.Conn);
                     cmd.ExecuteNonQuery();
                     this.Close();
@@ -60,8 +85,16 @@ namespace LibreriaDeLibrosSL
                     break;
                 case forma.eliminar:
                     break;
-
+                    s
             }
+            connexio.Close();
+        }
+
+        private void onSelectProvincia(object sender, SelectionChangedEventArgs e)
+        {
+            cb_editorial_popup_municipio.IsEnabled = true;
+            cb_editorial_popup_municipio.ItemsSource = 
+            ((sender as ComboBox).SelectedItem as Provincia).getMunicipios();
         }
     }
 }
---
+++ Provincia.cs
@@ -0,0 +1,48 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibreriaDeLibrosSL
+{
+    public class Provincia
+    {
+        public int ID { get; set; }
+        public String nombre { get; set; }
+
+        private List<Municipio> municipios;
+
+        public Provincia(int id, String nombre) {
+            this.ID = id;
+            this.nombre = nombre;
+
+
+            Connexio connexio = new Connexio();
+            string query = "SELECT id,municipi from municipis WHERE id_pro = " + this.ID;
+            MySqlCommand cmd = new MySqlCommand(query, connexio.Conn);
+            MySqlDataReader rdr = cmd.ExecuteReader();
+            //rdr.Read();
+            municipios = new List<Municipio>();
+
+            while (rdr.Read())
+            {
+                Municipio m = new Municipio(rdr.GetInt32(0), rdr.GetString(1));
+                municipios.Add(m);
+            }
+
+
+        }
+
+        public override string ToString()
+        {
+            return "[" + ID + "] " + nombre;
+        }
+
+        public List<Municipio> getMunicipios() {
+            return this.municipios;
+        }
+
+    }
+}
